d5607846fd47ff770a981a04ee71a19429f3e740
Oleksii996 <55384068+Oleksii996@users.noreply.github.com>
2025-12-02 14:37:34 +0000

    Deploying to gh-pages from @ Oleksii996/goit-js-lectures-1-12-modules@a39220ae0850905d03420461a87f0d4dad05a3e3 🚀

---
+++ module-10.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=new Promise((o,r)=>{console.log("create promise"),setTimeout(()=>{o("promise resolved")},5e3)}),t=setInterval(()=>{console.log(e)},1e3);e.then(()=>{clearInterval(t),console.log("interval stopped")});
+//# sourceMappingURL=module-10.js.map
